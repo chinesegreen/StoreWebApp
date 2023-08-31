a1d69e757d54537d92bd3b077360928dee016e3b
chinesegreen <lovelychinesegreen@gmail.com>
2023-08-31 13:27:45 +0300

    feat: initial project layout

---
+++ src/StoreWebApp.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreWebApp.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "StoreWebApp";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
