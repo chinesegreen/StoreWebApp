4ac06a7e34d2721fc29fe49dc224dedb894a3b3b
chineesegreen <lovelychinesegreen@gmail.com>
2023-09-03 01:17:10 +0300

    feat: add authentication pages and cookie

---
+++ src/Web/Configuration/ConfigureCookieSettings.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Web.Configuration;
+
+public static class ConfigureCookieSettings
+{
+    public const int ValidityMinutesPeriod = 60;
+    public const string IdentifierCookieName = "EshopIdentifier";
+
+    public static IServiceCollection AddCookieSettings(this IServiceCollection services)
+    {
+        services.Configure<CookiePolicyOptions>(options =>
+        {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                //TODO need to check that.
+                //options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+        });
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.EventsType = typeof(RevokeAuthenticationEvents);
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(ValidityMinutesPeriod);
+            options.LoginPath = "/Identity/Account/Login";
+            options.LogoutPath = "/Identity/Account/Logout";
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.SlidingExpiration = true;
+            options.Cookie = new CookieBuilder
+            {
+                Name = IdentifierCookieName,
+                IsEssential = true // required for auth to work without explicit user consent; adjust to suit your privacy policy
+                };
+        });
+
+        services.AddScoped<RevokeAuthenticationEvents>();
+
+        return services;
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,15 +1,84 @@
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+
+if (builder.Environment.IsDevelopment() || builder.Environment.EnvironmentName == "Docker")
+{
+    Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+}
+else
+{
+    builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+        options.UseNpgsql(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
+    });
+    builder.Services.AddDbContext<CatalogContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("CatalogConnection");
+        options.UseNpgsql(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
+    });
+}
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddDefaultUI()
+    .AddEntityFrameworkStores<AppIdentityDbContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.AddCookieSettings();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+    });
+
+//builder.Services.AddScoped<ITokenClaimsService, IdentityTokenClaimService>();
+//builder.Configuration.AddEnvironmentVariables();
+//builder.Services.AddCoreServices(builder.Configuration);
+//builder.Services.AddWebServices(builder.Configuration);
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+
+    // SignIn settings.
+    options.SignIn.RequireConfirmedEmail = false;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +87,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
