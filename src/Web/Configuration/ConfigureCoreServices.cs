fcb3c07d95f8c4a35e20a2e00db92b2e3e808e1e
chineesegreen <lovelychinesegreen@gmail.com>
2023-09-04 23:27:31 +0300

    feat: extend app services

---
+++ src/Web/Configuration/ConfigureCoreServices.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Configuration;
+
+public static class ConfigureCoreServices
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddDefaultUI()
     .AddEntityFrameworkStores<AppIdentityDbContext>()
         .AddDefaultTokenProviders();
+builder.Services.AddIdentityOptions();
 
 builder.Services.AddCookieSettings();
 
@@ -46,47 +47,32 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     });
 
 //builder.Services.AddScoped<ITokenClaimsService, IdentityTokenClaimService>();
-//builder.Configuration.AddEnvironmentVariables();
-//builder.Services.AddCoreServices(builder.Configuration);
-//builder.Services.AddWebServices(builder.Configuration);
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddCoreServices(builder.Configuration);
+builder.Services.AddWebServices(builder.Configuration);
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-
-    // SignIn settings.
-    options.SignIn.RequireConfirmedEmail = false;
-});
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
 {
+    app.Logger.LogInformation("Adding Development middleware...");
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.Logger.LogInformation("Adding non-Development middleware...");
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseCookiePolicy();
 app.UseAuthentication();
 app.UseAuthorization();
 
